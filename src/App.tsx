609870d1a6b982492d5532a236ebaee38bbca441
tyler <tyler@cruftbusters.com>
2021-05-31 14:34:11 -0500

    throw away cash for now

--- src/App.tsx
+++ src/App.tsx
@@ -81,8 +81,6 @@ function App() {
     )
   }, [])
 
-  const [cash, setCash] = useState(1000)
-
   useLayoutEffect(() => {
     if (canvasRef.current !== null) {
       const context = (canvasRef.current as HTMLCanvasElement).getContext(
@@ -138,33 +136,16 @@ function App() {
   }, [canvasRef, width, height, customers, towers])
 
   return (
-    <>
-      <div style={{ padding: '1em' }}>
-        <span
-          style={{
-            backgroundColor: '#222',
-            color: 'white',
-            padding: '1em',
-            borderRadius: '0.25em',
-          }}
-        >
-          Cash: {cash}
-        </span>
-      </div>
-      <canvas
-        ref={canvasRef}
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'block',
-          position: 'fixed',
-          top: 0,
-          zIndex: -1000,
-        }}
-        width={width}
-        height={height}
-      />
-    </>
+    <canvas
+      ref={canvasRef}
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'block',
+      }}
+      width={width}
+      height={height}
+    />
   )
 }
 
