bdf0288b9bf325bdba5328fbc1528d8bcfb0097b
tyler <tyler@cruftbusters.com>
2021-07-04 17:32:54 -0500

    bump parcels dataset version

--- src/fetch/fetchParcels.tsx
+++ src/fetch/fetchParcels.tsx
@@ -2,7 +2,7 @@ import 'google-protobuf'
 const { Parcels } = require('../proto/parcels_pb')
 
 function fetchParcels(): any | undefined {
-  return fetch('/parcels.pbf')
+  return fetch('/parcels.v2.pbf')
     .then((response) => response.arrayBuffer())
     .then((arrayBuffer) =>
       Parcels.deserializeBinary(new Uint8Array(arrayBuffer)),
